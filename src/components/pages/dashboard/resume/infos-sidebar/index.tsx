2aa9ef74e9397681c29744261d6be483f6f15bde
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-03-07 18:18:13 -0300

    feat: cria header sidebar info

--- src/components/pages/dashboard/resume/infos-sidebar/index.tsx
+++ src/components/pages/dashboard/resume/infos-sidebar/index.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import Logo from "@/assets/logo.svg";
+import { IaGenerationDropdown } from "./ia-generation-dropdown";
+import { Separator } from "@/components/ui/separator";
+
 export function InfosSidebar() {
-  return <aside className="w-full h-full p-6 overflow-auto">Teste</aside>;
+  return (
+    <aside className="w-full h-full p-6 overflow-auto">
+      <div className="w-full flex gap-4 flex-wrap items-center justify-between">
+        <Link href="/dashboard/resumes/">
+          <Logo className="w-fullw-[80px]   min-h-[38px] mx-auto" />
+        </Link>
+        <IaGenerationDropdown />
+      </div>
+      <Separator className="my-5" />
+    </aside>
+  );
 }
