854c4c575af83c4c3240666c1db1ab3a35adeb13
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-03-07 16:27:28 -0300

    feat: adiciona react-hook-form

--- src/app/dashboard/resumes/resumes-list/new-resume-dialog.tsx
+++ src/app/dashboard/resumes/resumes-list/new-resume-dialog.tsx
@@ -1,12 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { BaseDialogPros, Dialog } from "@/components/ui/dialog";
+import { InputField } from "@/components/ui/input";
+
+import React from "react";
+import { useForm, Resolver, FormProvider } from "react-hook-form";
+
+type FormData = {
+  title: string;
+};
+
+const resolver: Resolver<FormData> = async (values) => {
+  return {
+    values: values.title ? values : {},
+    errors: !values.title
+      ? {
+          title: {
+            type: "required",
+            message: "Esse campo é obrigatório.",
+          },
+        }
+      : {},
+  };
+};
 
 export function NewResumeDialog(props: BaseDialogPros) {
+  const methods = useForm<FormData>({ resolver });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
   return (
     <Dialog
       {...props}
       title="Criar novo currículo"
       description="Para começar, escolha um título para seu currículo"
-      content={<form></form>}
+      content={
+        <FormProvider {...methods}>
+          <form
+            className="flex flex-col"
+            onSubmit={methods.handleSubmit(onSubmit)}
+          >
+            <InputField label="Título" name="title" />
+            <Button className="w-max mt-6 ml-auto">Criar</Button>
+          </form>
+        </FormProvider>
+      }
     />
   );
 }
---
+++ src/components/ui/input/index.tsx
@@ -0,0 +1,27 @@
+import { Controller, useFormContext } from "react-hook-form";
+import { Input } from "./input";
+import { ComponentProps } from "react";
+import { FieldWrapper } from "../field-wrapper";
+type InputFieldProps = ComponentProps<typeof Input> & {
+  label: string;
+  name: string;
+};
+
+export function InputField({ label, name, ...props }: InputFieldProps) {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState }) => (
+        <FieldWrapper label={label}>
+          <Input {...props} {...field} />
+          {fieldState.error && (
+            <p className="text-sm text-red-500">{fieldState.error.message}</p>
+          )}
+        </FieldWrapper>
+      )}
+    />
+  );
+}
