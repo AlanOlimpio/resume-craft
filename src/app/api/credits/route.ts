a7e8a01b74157c41e3f37d708a6c4f1c9bdb6df7
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-03-28 14:20:42 -0300

    feat: cria endpoint para events stripe Webhook
    Documentação do webhook https://docs.stripe.com/webhooks  e Stripe CLI https://docs.stripe.com/stripe-cli?install-method=apt é necessário instalar a Stripe CLI para testar local.

--- src/app/api/credits/route.ts
+++ src/app/api/credits/route.ts
@@ -3,7 +3,5 @@ import { getUserCredits } from "@/db/queries";
 export const GET = async () => {
   const credits = await getUserCredits();
 
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-
   return Response.json({ credits });
 };
--- src/components/shared/client-providers.tsx
+++ src/components/shared/client-providers.tsx
@@ -8,6 +8,22 @@ import { Toaster } from "../ui/sonner";
 import { useSearchParams } from "next/navigation";
 import { toast } from "sonner";
 
+const CreditsToast = () => {
+  const searchParams = useSearchParams();
+
+  const successCheckoutParam = searchParams.get("success");
+
+  useEffect(() => {
+    if (successCheckoutParam === "true") {
+      toast.success(
+        "Compra realizada com sucesso! Seus créditos foram adicionados à sua conta."
+      );
+    }
+  }, [successCheckoutParam]);
+
+  return null;
+};
+
 type ClientProvidersProps = {
   children: ReactNode;
 };
@@ -23,6 +39,9 @@ export const ClientProviders = ({ children }: ClientProvidersProps) => {
         enableSystem
         disableTransitionOnChange
       >
+        <Suspense>
+          <CreditsToast />
+        </Suspense>
         {children}
         <Toaster />
       </ThemeProvider>
